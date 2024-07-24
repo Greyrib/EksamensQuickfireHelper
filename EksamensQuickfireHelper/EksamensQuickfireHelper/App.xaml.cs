3822abd3c6e87460883f663f0b7627fdc2f8c07f
Michael Leicht Abildgaard <MLAbildgaard@live.dk>
2024-07-24 12:14:00 +0200

    First upload!

---
+++ EksamensQuickfireHelper/EksamensQuickfireHelper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EksamensQuickfireHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
